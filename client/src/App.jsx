48486f9a73e54826a3de890507d508ed387af436
priyanshu-sahani-10 <priyanshusahani038@gmail.com>
2025-12-27 18:44:14 +0530

    Testing..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/pages/Layout';
 import Marketplace from './components/pages/Marketplace';
 import MyProducts from './components/pages/MyProducts';
-import Orders from './components/pages/Orders';
 import Chats from './components/pages/Chats';
 import RegisterCrop from './components/pages/RegisterCrop';
 import Profile from './components/pages/Profile';
@@ -37,7 +36,6 @@ export default function App() {
         <Route path="marketplace" element={<Marketplace/>}/>
         <Route path="marketplace/:cropId" element={<SingleCrop/>}/>
         <Route path="my-products" element={<MyProducts/>}/>
-        <Route path="orders" element={<Orders/>}/>
         <Route path="chat" element={<Chats/>}/>
         <Route path="register-crop" element={<RegisterCrop/>}/>
         <Route path="profile" element={<Profile/>}/>
